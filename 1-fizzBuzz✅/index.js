ccf4c2ce2e544511088a34f3192077bad1a3914c
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2023-05-18 15:04:03 +0300

    Fizz Buzz solved

---
+++ 1-fizzBuzz✅/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let out=[]
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 !== 0) {
+            out.push("Fizz")
+            //console.log("Fizz")
+        } else if (i % 3 !== 0 && i % 5 === 0) {
+            out.push("Buzz")
+            //console.log("Buzz")
+        } else if (i % 3 === 0 && i % 5 === 0) {
+            out.push("FizzBuzz")
+            //console.log("FizzBuzz")
+        } else {
+            out.push(`${i}`)
+        }
+    }
+    return out
+};
