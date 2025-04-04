cd36e142d6e26ac9aa162e29c5618c4e1dbd0eda
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2025-04-04 22:31:34 +0200

    Can Place Flowers solved ✅

---
+++ 00037-Can-Place-Flowers✅/index.js
@@ -0,0 +1,14 @@
+var canPlaceFlowers = function(flowerbed, n) {
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0 &&
+            (i === 0 || flowerbed[i - 1] === 0) &&
+            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+        ) {
+            flowerbed[i] = 1; 
+            n--; 
+        }
+
+        if (n === 0) return true; 
+    }
+    return n <= 0; 
+};
