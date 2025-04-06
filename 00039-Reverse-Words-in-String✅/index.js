2b5b3349e08e18f519c9f3a3ee648b0c7c955afb
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2025-04-06 06:43:11 +0200

    Reverse Words in a String solved ✅

---
+++ 00039-Reverse-Words-in-String✅/index.js
@@ -0,0 +1,10 @@
+var s = "the sky    is blue";
+
+var reverseWords = function (s) {
+
+    return reversedString = s.trim().split(/\s+/).reverse().join(" ");
+
+};
+
+
+console.log(reverseWords(s));
